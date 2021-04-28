44d55f5ee353eb25b3b5c2dd7b569e950c47c7d4
luna <hoanglongnguyen.letter@gmail.com>
2021-04-28 22:12:30 +0700

    add various things again and again

---
+++ src/data.js
@@ -0,0 +1,15 @@
+const songs = [
+    "Robber and Bouquet",
+    "Spring Thief",
+    "Creation",
+    "Eat the Wind",
+    "Liar",
+  ];
+  
+  const artists = [
+    "Yorushika",
+    "Yorushika",
+    "Yorushika",
+    "Yorushika",
+    "Yorushika",
+  ];
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const audio = document.querySelector("#audio");
 const progress = document.querySelector(".progress");
 const progressContainer = document.querySelector(".progress-container");
 const title = document.querySelector("#title");
+const artistTitle = document.querySelector("#artist");
 const cover = document.querySelector("#cover");
 
 function openNav() {
@@ -18,26 +19,23 @@ function closeNav() {
   document.getElementById("main").style.marginLeft = "0";
 }
 
-// Song titles
-const songs = [
-  "Burglar and Bouquet",
-  "Spring Thief",
-  "Creation",
-  "Eat the Wind",
-  "Liar",
-];
-
-// Keep track of songs
+// Default track of songs
 let songIndex = songs.length - 1;
+let artistIndex = artists.length - 1;
 
 // Initially load song info DOM
 loadSong(songs[songIndex]);
+loadArtist(artists[artistIndex]);
 
 // Update song details
 function loadSong(song) {
   title.innerText = song;
   audio.src = `./music/${song}.mp3`;
   cover.src = `./img/${song}.jpg`;
+}
+
+function loadArtist(artist) {
+  artistTitle.innerText = artist;
 }
 
 // Play song
@@ -60,17 +58,45 @@ function pauseSong() {
 // Change song
 function prevSong() {
   songIndex--;
+  artistIndex--;
 
-  if(songIndex < 0) {
+  if (songIndex < 0) {
     songIndex = songs.length - 1;
+    artistIndex = songs.length - 1;
   }
 
   loadSong(songs[songIndex]);
+  loadArtist(artists[artistIndex]);
   playSong();
 }
 
 function nextSong() {
+  songIndex++;
+  artistIndex++;
 
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+    artistIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+  loadArtist(artists[artistIndex]);
+  playSong();
+}
+
+// Progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
 }
 
 // Event Listeners
@@ -84,6 +110,9 @@ playBtn.addEventListener("click", () => {
   }
 });
 
-// Change song events
+// Song events
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+audio.addEventListener("timeupdate", updateProgress);
+progressContainer.addEventListener("click", setProgress);
+audio.addEventListener("ended", nextSong);
