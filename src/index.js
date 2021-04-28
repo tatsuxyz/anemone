4d2cafdf51283f6ef703c3c6786c36fb7b441f4c
luna <hoanglongnguyen.letter@gmail.com>
2021-04-28 17:07:24 +0700

    add various things

--- src/index.js
+++ src/index.js
@@ -1,9 +1,89 @@
+const musicContainer = document.querySelector(".player-dock");
+const playBtn = document.querySelector("#play");
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+const audio = document.querySelector("#audio");
+const progress = document.querySelector(".progress");
+const progressContainer = document.querySelector(".progress-container");
+const title = document.querySelector("#title");
+const cover = document.querySelector("#cover");
+
 function openNav() {
-    document.getElementById("mySideBar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySideBar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySideBar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+// Song titles
+const songs = [
+  "Burglar and Bouquet",
+  "Spring Thief",
+  "Creation",
+  "Eat the Wind",
+  "Liar",
+];
+
+// Keep track of songs
+let songIndex = songs.length - 1;
+
+// Initially load song info DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `./music/${song}.mp3`;
+  cover.src = `./img/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+}
+
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+}
+
+// Change song
+function prevSong() {
+  songIndex--;
+
+  if(songIndex < 0) {
+    songIndex = songs.length - 1;
   }
-  
-  function closeNav() {
-    document.getElementById("mySideBar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+
+}
+
+// Event Listeners
+playBtn.addEventListener("click", () => {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
   }
+});
+
+// Change song events
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
