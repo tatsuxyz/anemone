40b517adab99251565bf0e3a54a40ad3f607fc20
giang00yb <ggvu2017@gmail.com>
2021-05-01 14:14:05 +0700

    update

--- src/dataArtis.js
+++ src/dataArtis.js
@@ -1,21 +1,102 @@
+// const data = [
+//   {
+//     name: "Nguyễn Thanh Tùng",
+//     dob: "5-7-1994",
+//     stageName: "Sơn Tùng MTP",
+//     infor: "Được mệnh danh là `Hoàng tử V - pop`, Sơn Tùng M-TP được nhiều nhà báo âm nhạc, nhà lý luận phê bình và tác giả xem là một trong những nghệ sĩ V-pop thành công nhất ở thời điểm hiện tại. Trong suốt sự nghiệp của mình, Tùng nhận được nhiều giải thưởng trong nước và quốc tế, bao gồm một giải thưởng Âm nhạc Cống hiến, một giải Âm nhạc châu Âu của MTV, một giải Mnet Asian Music Awards, bảy giải Làn Sóng Xanh và được xếp vào danh sách 30 Under 30 của tạp chí Forbes Vietnam.",
+    
+//   },
+//   {
+//     name: "Trần Anh Quân",
+//     dob: "21-9-1993",
+//     stageName: "Jaykii",
+//     infor: "Trần Anh Quân từng xuất sắc lọt vào TOP 10 VietNam Idol 2013. Dừng chân lại ở TOP 10 nhưng Anh Quân đã nhận được rất nhiều tình cảm của khán giả. Trần Anh Quân đã đổi nghệ danh sang JayKii.Chia sẻ về nghệ danh mới, nam ca sĩ chia sẻ anh muốn sự nghiệp của mình sẽ bước sang một trang mới với cái tên mới. Sở dĩ anh lấy tên JayKii vì anh là một fan cứng của giám khảo Asia's Got Talent 2017 - Jay Park. JayKii sở hữu chiều cao lý tưởng cùng khuôn mặt điển trai, nụ cười duyên khiến không ít fan nữ ôm tim rung rinh. ",
+//   }
+// ];
+
+// console.log(data[0]);
+
+
+// const nameA = document.getElementById("nameA");
+// const dob = document.getElementById("dob");
+// const stageName = document.getElementById("stageName");
+// const infor = document.getElementById("infor");
+
+
+// const artisName = document.querySelector("#nameArtis");
+
+// loadName(dataName[nameAIndex]);
+// loadDob(dataDob[dobIndex]);
+// loadStageName(dataStageName[stageNameIndex]);
+// loadInfor(dataInfor[inforIndex]);
+
+
+// function loadName(dataName) {
+//   nameA.innerText = dataName;
+// }
+
+// function loadDob(dataDob) {
+//   dob.innerText = dataDob;
+// }
+
+// function loadStageName(dataStageName) {
+//   stageName.innerText = dataStageName;
+// }
+
+// function loadInfor(dataInfor) {
+//   infor.innerText = dataInfor;
+// }
+
+// function showInfor(value) {
+//   for (let i = 0; i < data.length; i++) {
+//     if(i==value){
+//       let artisName = data[i].name;
+//       let artisDob = data[i].dob;
+//       let stageName = data[i].stageName;
+//       let info = data[i].info;
+//       document.getElementById("name").innerText = artisName;
+//       document.getElementById("dob").innerText = artisDob;
+//       document.getElementById("stageName").innerText = stageName;
+//       document.getElementById("infor").innerText = info;
+//     }
+//   }
+// }
+
+
 const data = [
-    {
-        name: "Nguyễn Thanh Tùng",
-        dob: "5-7-1994",
-        stageName: "Sơn Tùng MTP",
-        info: "Được mệnh danh là `Hoàng tử V - pop`, Sơn Tùng M-TP được nhiều nhà báo âm nhạc, nhà lý luận phê bình và tác giả xem là một trong những nghệ sĩ V-pop thành công nhất ở thời điểm hiện tại. Trong suốt sự nghiệp của mình, Tùng nhận được nhiều giải thưởng trong nước và quốc tế, bao gồm một giải thưởng Âm nhạc Cống hiến, một giải Âm nhạc châu Âu của MTV, một giải Mnet Asian Music Awards, bảy giải Làn Sóng Xanh và được xếp vào danh sách 30 Under 30 của tạp chí Forbes Vietnam."
-    },
-    {
-        name: "Trần Anh Quân",
-        dob: "21-9-1993",
-        stageName: "Jaykii",
-        info: "Trần Anh Quân từng xuất sắc lọt vào TOP 10 VietNam Idol 2013. Dừng chân lại ở TOP 10 nhưng Anh Quân đã nhận được rất nhiều tình cảm của khán giả. Trần Anh Quân đã đổi nghệ danh sang JayKii.Chia sẻ về nghệ danh mới, nam ca sĩ chia sẻ anh muốn sự nghiệp của mình sẽ bước sang một trang mới với cái tên mới. Sở dĩ anh lấy tên JayKii vì anh là một fan cứng của giám khảo Asia's Got Talent 2017 - Jay Park. JayKii sở hữu chiều cao lý tưởng cùng khuôn mặt điển trai, nụ cười duyên khiến không ít fan nữ ôm tim rung rinh. "
+  "M-TP tên thật là Nguyễn Thanh Tùng.Cậu thanh niên sinh năm 1994 ở Thái Bình sớm bị hip hop hớp hồn giống như bao bạn bè đồng trang lứa. Và với niềm đam mê này, M-TP quyết tâm khăn gói tới Hà Nội học hỏi thêm về hip hop. Sau khi tốt nghiệp cấp 3, anh chàng dự định sẽ đầu quân làm học viên tại Học viện M4Me để rèn dũa khả năng ca hát, sáng tác... trước khi chính thức theo đuổi con đường âm nhạc.Ngoài đam mê ca hát, M-TP còn có khả năng sáng tác, chơi piano và nhảy cực đỉnh. Với thế mạnh này, anh chàng không ngừng cố gắng học tập các bậc đàn anh đàn chị và đã có trong tay một hành trang khá khủng những sáng tác của riêng mình.Sự nghiệp âm nhạc:Tháng 8/2011: M-TP phát hành ca khúc Cơn Mưa Ngang Qua lên mạng, ngay lập tức nó trở thành một ca khúc hot lúc bấy giờ khi mà chỉ sau hai tháng, đã có hơn 1.7 triệu lượt nghe - con số mà những ca sĩ kỳ cựu như Đàm Vĩnh Hưng, Hồ Ngọc Hà... cũng phải mơ ước.Năm 2012, với tổng điểm 25,5đ, M-TP trở thành thủ khoa Nhạc viện TP.HCM. Cậu chia sẻ rằng chia sẻ cảm xúc: Mặc dù khá tự tin sau khi kết thúc buổi thi nhưng mình vẫn không khỏi ngạc nhiên và hạnh phúc khi biết được kết quả thi. Đây thực sự là một món quà vô cùng ý nghĩa để bù đắp cho những nỗ lực của mình trong suốt thời gian ôn luyện vừa quaĐể lại nhiều ấn tượng trong lòng khán giả khi M-TP trình diễn thành công ca khúc Cơn Mưa Ngang Qua tại Bài Hát Yêu Thích tháng 10/2012 và tiếp tục nhận giải thưởng hát hát yêu thích của tháng trong chương trình là động lực rất lớn cho M-TP tiếp tục phấn đấu trong con đường âm nhạc chuyên nghiệp. Sau Bài Hát Yêu Thích, M-TP còn được mời tham gia biểu diễn trong đêm công bố kết quả Top 9 Vietnam Idol 2012.Tại Zing Mp3, Cơn Mưa Ngang Qua như là cú hít đầu tiên của cậu ca sĩ trẻ M-TP bước vào làng nhạc trẻ. Tính đến tháng 11/2014, ca khúc đạt được 64,278,735 lượt nghe.Năm 2013, anh đầu quân về công ty Văn Production JSC của nhạc sĩ Huy Tuấn và chính thức đổi nghệ danh thành Sơn Tùng M-TP để hoạt động âm nhạc chuyên nghiệp.",
+  "Là một nhạc sỹ có khả năng tự trình bày những ca khúc do chính anh sáng tác được khán giả đặc biệt yêu thích. Từ năm 2008, Mr. Siro sớm có được những bài HIT đầu tay như Em, Melancholy, Mây, Giấc Mơ Của Anh, I miss you từ năm cho đến gần đây (2012, 2013, 2014) Mr. Siro lập HIT liên tục với những sáng tác liên tiếp đứng đầu các bảng xếp hạng trong suốt nhiều tuần liền như Lắng Nghe Nước Mắt, Bức Tranh Từ Nước Mắt, Không Cần Thêm Một Ai Nữa (ft BigDaddy), Em Đã Sai Vì Em Tin (Bích Phương). Gần đây nhất là bài Em Gái Mưa (Hương Tràm) đang nằm trong top bảng xếp hạng của Zing MP3.Năm 2013, anh đạt nhiều giải thưởng quan trọng do Làn Sóng Xanh và Zing Music Award trao giúp con đường âm nhạc của ngày ngày một rộng mở hơn.Vô Hình Trong Tim Em là một sáng tác mà Mr. Siro muốn làm mới dòng nhạc sở trường của mình, hiện đại hơn so với những bài ballad êm dịu trước đó nhưng vẫn giữ được sự sâu sắc trong ca từ và sự mượt mà trong giai điệu. Ca khúc vừa tung ra đã nhanh chóng tạo được sức ảnh hưởng trên cộng đồng mạng khi liên tục được khán giả chia sẻ và đăng tải trên các mạng xã hội.Âm nhạc mr. Siro luôn nhẹ nhàng, nhưng lại da diết và mang cảm giác day dứt, ám ảnh rất sâu mỗi khi nghe. Ngoài những bản tình ca luôn chạm được đến trái tim khán giả, Mr. Siro còn là người thầy rất có tâm, có trách nhiệm đặc biệt đối với những tài năng âm nhạc trẻ mà anh đang dẫn dắt. Trong thời gian sắp tới mr. Siro hy vọng sẽ tiếp tục gặt hái được nhiều thành công hơn nữa.",
+  "Karik tham gia nhóm nhảy Freestyle năm 2006 nhưng 2008 phải nghỉ vì chấn thương. Anh chuyển sang Rap để giải tỏa cảm xúc.Ban đầu, Karik tự mình mày mò từ flow, lyrics và beat cho đến cách tự thu âm và dần dần được cộng đồng Underground công nhận sau cuối năm 2009.Năm 2012 anh vinh dự là nghệ sĩ nhạc Rap đầu tiên đoạt giải MTV Việt Nam.Các bài hát phát hành sau đó đều được khán giả yêu thích như: 'Anh Không Đòi Quà', 'Ế', 'Rắc Rối'...Năm 2018 Karik kết hợp cùng Orange trong bài hát 'Người Lạ Ơi', bài hát trở thành hiện tượng của năm. Sau đó, cả hai còn hợp tác trong 'Mình Từng Yêu Như Thế' và 'Vô Thường'.Đầu năm 2019, Karik tung ra sản phẩm đậm chất cá nhân mang tên 'Anh Em Tao'.",
+  "Hương Tràm là con nhà nòi. Cô có ba là NSƯT Tiến Dũng và anh trai là thí sinh Sao Mai điểm hẹn 2010 Phạm Tiến Mạnh.Hương Tràm đã gây cơn sốt trong buổi lên sóng đầu tiên ở vòng Giấu Mặt của The Voice 2012 khi cô chọn ca khúc kinh điển I Will Always Love You của Whitney Houston và đã được cả 4 vị huấn luyện viên xoay ghế chọn vào đội của mình. Tuy nhiên, Hương Tràm quyết định chọn ca sỹ Thu Minh là vị huấn luyện viên cho giọng ca của mình.Ngày 13/01/2013, Hương Tràm chính thức trở thành quán quân đầu tiên của Giọng Hát Việt. Với ngôi vị quán quân, cô gái Nghệ An sẽ nhận được giải thưởng 500 triệu đồng tiền mặt, 200 triệu tiền sản phẩm của nhà tài trợ và một chuyến du lịch Hàn Quốc 7 ngày. Ngoài ra, cô còn được sở hữu một hợp đồng ghi âm khủng với hãng đĩa quốc tế.",
+];
+
+// console.log(data[0]);
+
+// let index = 0;
+// let infor = document.getElementById("infor");
+
+// loadInfor(data[index]);
+
+// function loadInfor(Data) {
+//   infor.innerText = Data;
+// }
+
+function showInfor(value) {
+  document.getElementById('id01').style.display = 'block';
+  for(i=0; i<data.length; i++){
+    if(i == value){
+      infor.innerText = data[i];
+      img
+    }else{
+      console.log("Error");
     }
-]
-
-console.log(data[1].name);
-
-function getData(){
-    let displayInfor;
-    displayInfor = document.getElementById("infor").data[0].name;
+  }
 }
+  // function show() {
+    
+  //   showInfor(value);
+  // }
+
+  function closeInfor() {
+    document.getElementById('id01').style.display = 'none';
+  }
